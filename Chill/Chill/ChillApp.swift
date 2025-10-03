3daedb7d52fae9b4cc5c3e0aa299269ffb68dddd
Jin Budelmann <jin@bitcrank.com>
2025-10-04 11:01:13 +1300

    Create iOS project

---
+++ Chill/Chill/ChillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillApp.swift
+//  Chill
+//
+//  Created by Jin Budelmann on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChillTests/ChillTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChillTests.swift
+//  ChillTests
+//
+//  Created by Jin Budelmann on 10/4/25.
+//
+
+import Testing
+@testable import Chill
+
+struct ChillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
